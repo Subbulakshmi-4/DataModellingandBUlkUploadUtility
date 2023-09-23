22f4c48a20fba267e265b340441d627014c78a01
Subbulakshmi-4 <121540421+Subbulakshmi-4@users.noreply.github.com>
2023-09-23 16:35:00 +0530

    Base_Commit
    Base_Commit

---
+++ DMU_Git/DMU_Git/Models/BaseModel.cs
@@ -0,0 +1,14 @@
+﻿namespace DMU_Git.Models
+{
+    public class BaseModel
+    {
+        public DateTime CreatedDate { get; set; }
+
+        public int CreatedBy { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+        public int UpdatedBy { get; set; }
+
+    }
+}
