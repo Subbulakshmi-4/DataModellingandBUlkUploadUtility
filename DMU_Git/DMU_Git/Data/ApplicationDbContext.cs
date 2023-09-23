2f3bacf5a050e54df4448bd98492174a377437e5
Subbulakshmi-4 <121540421+Subbulakshmi-4@users.noreply.github.com>
2023-09-23 15:43:25 +0530

    Base_Commit
    Base_Commit for model files

---
+++ DMU_Git/DMU_Git/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DMU_Git.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DMU_Git.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+                
+        }
+
+        public DbSet<EntityListMetadataModel> EntityListMetadataModels { get; set; }
+
+        public DbSet<EntityColumnListMetadataModel> EntityColumnListMetadataModels { get; set; }
+    }
+}
---
+++ DMU_Git/DMU_Git/Models/EntityListMetadataModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DMU_Git.Models
+{
+    public class EntityListMetadataModel :BaseModel
+    {
+        public int Id { get; set; }
+        public string EntityName { get; set; }
+    }
+}
