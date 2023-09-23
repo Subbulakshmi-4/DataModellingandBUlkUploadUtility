df534865eb38e037e53cafceece6db3d42de50c2
Subbulakshmi-4 <121540421+Subbulakshmi-4@users.noreply.github.com>
2023-09-23 16:31:18 +0530

    Delete DMU_Git directory

--- DMU_Git/DMU_Git/Models/EntityListMetadataModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DMU_Git.Models
-{
-    public class EntityListMetadataModel :BaseModel
-    {
-        public int Id { get; set; }
-        public string EntityName { get; set; }
-    }
-}
