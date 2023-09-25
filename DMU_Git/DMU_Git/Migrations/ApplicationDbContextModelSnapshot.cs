623feb094b7b0fa3cb6e4c216ab7c36811641b3c
MounikaSubash <MounikaSubash@DESKTOP-TQ6D7NE>
2023-09-25 18:53:53 +0530

    AddMigration

--- DMU_Git/DMU_Git/Migrations/ApplicationDbContextModelSnapshot.cs
+++ DMU_Git/DMU_Git/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -68,6 +68,8 @@ namespace DMU_Git.Migrations
 
                     b.HasKey("Id");
 
+                    b.HasIndex("EntityId");
+
                     b.ToTable("EntityColumnListMetadataModels");
                 });
 
@@ -98,6 +100,20 @@ namespace DMU_Git.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("EntityListMetadataModels");
+                });
+
+            modelBuilder.Entity("DMU_Git.Models.EntityColumnListMetadataModel", b =>
+                {
+                    b.HasOne("DMU_Git.Models.EntityListMetadataModel", null)
+                        .WithMany("EntityColumnListMetadata")
+                        .HasForeignKey("EntityId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+                });
+
+            modelBuilder.Entity("DMU_Git.Models.EntityListMetadataModel", b =>
+                {
+                    b.Navigation("EntityColumnListMetadata");
                 });
 #pragma warning restore 612, 618
         }
