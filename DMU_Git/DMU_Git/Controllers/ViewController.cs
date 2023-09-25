2abeba729b388a278691147dac34ec8f428d6130
MounikaSubash <MounikaSubash@DESKTOP-TQ6D7NE>
2023-09-25 13:05:39 +0530

    AddingView_Excel_Controller

--- DMU_Git/DMU_Git/Controllers/ViewController.cs
+++ DMU_Git/DMU_Git/Controllers/ViewController.cs
@@ -1,9 +1,13 @@
-﻿using DMU_Git.Services;
+﻿using DMU_Git.Models.DTO;
+using DMU_Git.Services;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace DMU_Git.Controllers
 {
     [Route("api/entity")]
+    [EnableCors("AllowAngular")]
     [ApiController]
     public class ViewController : Controller
     {
@@ -17,14 +21,30 @@ namespace DMU_Git.Controllers
         [HttpGet("{entityName}/columns")]
         public IActionResult GetColumnsForEntity(string entityName)
         {
-            var columns = _viewService.GetColumnsForEntity(entityName);
+            var columnsDTO = _viewService.GetColumnsForEntity(entityName);
 
-            if (columns == null)
+            if (columnsDTO == null)
             {
-                return NotFound();
+                var response = new APIResponse<List<EntityColumnDTO>>
+                {
+                    StatusCode = HttpStatusCode.NotFound,
+                    IsSuccess = false,
+                    ErrorMessage = new List<string> { "Columns not found" },
+                    Result = null
+                };
+                return NotFound(response);
             }
 
-            return Ok(columns);
+            var apiResponse = new APIResponse<List<EntityColumnDTO>>
+            {
+                StatusCode = HttpStatusCode.OK,
+                IsSuccess = true,
+                ErrorMessage = null,
+                Result = (List<EntityColumnDTO>)columnsDTO
+            };
+
+            return Ok(apiResponse);
         }
+
     }
 }
--- DMU_Git/DMU_Git/Services/ViewService.cs
+++ DMU_Git/DMU_Git/Services/ViewService.cs
@@ -1,5 +1,6 @@
 ﻿using DMU_Git.Data;
 using DMU_Git.Models;
+using DMU_Git.Models.DTO;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 
@@ -14,12 +15,29 @@ namespace DMU_Git.Services
             _context = context;
         }
 
-        public IEnumerable<EntityColumnListMetadataModel> GetColumnsForEntity(string entityName)
+        public IEnumerable<EntityColumnDTO> GetColumnsForEntity(string entityName)
         {
             var entity = _context.EntityListMetadataModels.Include(e => e.EntityColumnListMetadata) // Include related columns
                 .FirstOrDefault(e => e.EntityName == entityName);
 
-            return entity?.EntityColumnListMetadata;
+            if (entity == null)
+            {
+                return null;
+            }
+
+            // Manual mapping to DTO
+            var columnsDTO = entity.EntityColumnListMetadata.Select(column => new EntityColumnDTO
+            {
+                Id = column.Id,
+                EntityColumnName = column.EntityColumnName,
+                Datatype = column.Datatype,
+                Length = column.Length,
+                IsNullable = column.IsNullable,
+                DefaultValue = column.DefaultValue,
+                ColumnPrimaryKey = column.ColumnPrimaryKey
+            }).ToList();
+
+            return columnsDTO;
         }
     }
 }
