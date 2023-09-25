e7d575f77d68b6a122cdaf6aa1fb8037d055dcd0
Subbulakshmi Sudalaimuthu <Subbulakshmi.S@datayaan.com>
2023-09-25 10:55:04 +0530

    List The Entitylist using Metadata

---
+++ DMU_Git/DMU_Git/Models/DTO/EntityListDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DMU_Git.Models.DTO
+{
+    public class EntityListDto
+    {
+        //public int Id { get; set; }
+        public string EntityName { get; set; }
+    }
+}
--- DMU_Git/DMU_Git/Program.cs
+++ DMU_Git/DMU_Git/Program.cs
@@ -1,4 +1,6 @@
 using DMU_Git.Data;
+using DMU_Git.Services;
+using DMU_Git.Services.Interface;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +11,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularDev",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:4200")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IEntitylistService, EntitylistService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,7 +31,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+// Apply CORS policy
+app.UseCors("AllowAngularDev");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
