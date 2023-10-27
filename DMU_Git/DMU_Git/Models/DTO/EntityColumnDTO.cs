2e3265d2711fdc4f21519b67a3d3875ffaa61454
MounikaSubash <MounikaSubash@DESKTOP-TQ6D7NE>
2023-10-27 14:55:25 +0530

    TemplateChanges_MinandMax

--- DMU_Git/DMU_Git/Models/DTO/EntityColumnDTO.cs
+++ DMU_Git/DMU_Git/Models/DTO/EntityColumnDTO.cs
@@ -12,6 +12,10 @@ namespace DMU_Git.Models.DTO
         public int EntityId { get; set; }
         public string Datatype { get; set; }
         public int Length { get; set; }
+        public int? MinLength { set; get; }
+        public int? MaxLength { set; get; }
+        public string DateMinValue { set; get; }
+        public string DateMaxValue { set; get; }
         public string Description { get; set; } // New property
         public bool IsNullable { get; set; }
         public string DefaultValue { get; set; }
@@ -30,6 +34,10 @@ namespace DMU_Git.Models.DTO
                 EntityId = data.EntityId,
                 Datatype = data.Datatype,
                 Length = data.Length,
+                MinLength = data.MinLength,
+                MaxLength = data.MaxLength,
+                DateMinValue = data.DateMinValue,
+                DateMaxValue = data.DateMaxValue,
                 Description = data.Description, // Map the Description property
                 IsNullable = data.IsNullable,
                 DefaultValue = data.DefaultValue,
@@ -48,6 +56,10 @@ namespace DMU_Git.Models.DTO
                 EntityId = data.EntityId,
                 Datatype = data.Datatype,
                 Length = data.Length,
+                MinLength = data.MinLength,
+                MaxLength = data.MaxLength,
+                DateMinValue = data.DateMinValue,
+                DateMaxValue = data.DateMaxValue,
                 Description = data.Description, // Map the Description property
                 IsNullable = data.IsNullable,
                 DefaultValue = data.DefaultValue,
