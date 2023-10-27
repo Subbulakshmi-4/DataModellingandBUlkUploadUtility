cbcb5435b6e9b0a8d91ee551b5b0995dc3a4cc07
Saravanan09d <111735144+Saravanan09d@users.noreply.github.com>
2023-10-27 11:53:21 +0530

    CreateTableandAlignmentChanges

--- DMU_Git/DMU_Git/Models/BaseModel.cs
+++ DMU_Git/DMU_Git/Models/BaseModel.cs
@@ -3,12 +3,8 @@
     public class BaseModel
     {
         public DateTime CreatedDate { get; set; }
-
         public int CreatedBy { get; set; }
-
         public DateTime UpdatedDate { get; set; }
-
         public int UpdatedBy { get; set; }
-
     }
 }
